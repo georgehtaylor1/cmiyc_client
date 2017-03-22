f08490e358d9bdb4a2730288cdfd8875f9958b4b
Harvey Hyatt <downthedrain97@live.co.uk>
2017-03-22 15:11:03 +0000

    but he does have bluetooth

--- src/com/ClientSender.java
+++ src/com/ClientSender.java
@@ -5,85 +5,74 @@ import java.util.HashMap;
 import constants.Commands.Action;
 import constants.Commands.Key;
 import game.Faction;
-import launcher.Main;
+import states.ClientState;
+import util.Client;
 import util.Debug;
 import util.Transferable;
+import util.Client.ConnectionState;
 
-import com.abstracts.AbstractSender;
+public class ClientSender implements Runnable {
 
-public class ClientSender extends AbstractSender {
+	private volatile Client client;
+	
+	public volatile Object monitor;
+	
+	private int exhaution = 10;
 
-	public ClientSender(Main _client) {
-		super(_client);
-	}
+	public ClientSender( Client _client) { this.client = _client; }
 
 	@Override
 	public void run() {
 
 		Debug.say("Sender Started for [ " + this.client.id + " ]");
-		this.running = true;
 
-		while (this.running) {
-
-			if (!this.client.emptyQueue()) {
-				this.send(this.client.shiftQueue());
-			}
-			if (this.movingOnline()) {
-				this.send(this.buildMovement());
+		while( this.client.connectionState == ConnectionState.CONNECTED ) {
+			
+			if( ( this.client.queue.isEmpty() ) && ( this.client.state == ClientState.IDLE ) ) {
+				synchronized( this.monitor ) {
+					try { this.monitor.wait(); } catch( Exception _exception ) { /* God knows. */ }
+				}
 			}
 
+			if( !this.client.queue.isEmpty() ) { this.send( this.client.queue.poll() ); }
+			if( this.needsPosition() ) { this.send( this.buildMovement() ); }
+			
 		}
 
 	}
 
-	@Override
-	public void stopSending() {
-		this.running = false;
+	private boolean needsPosition() {
+		switch (this.client.getState()) {
+		case FINDING: return true;
+		case PREGAME: return true;
+		case PLAYING: return true;
+		default: return false;
+		}
 	}
 
-	@Override
-	protected Transferable buildMovement() {
+	private Transferable buildMovement() {
 
 		Action action = Action.UPDATE_MOVEMENT;
 		HashMap<Key, Object> map = new HashMap<Key, Object>();
 
-		map.put(Key.POSITION, this.client.player.position);
+		map.put( Key.POSITION, this.client.player.position );
 
 		if (this.client.player.faction == Faction.SECURITY) {
 			map.put(Key.DIRECTION, this.client.player.direction);
+			map.put( Key.BATTERY, this.client.player.battery );
 		}
 
 		return (new Transferable(action, map));
 
 	}
 
-	@Override
-	protected boolean movingOnline() {
-		switch (this.client.state) {
-		case FINDING:
-			return true;
-		case PREGAME:
-			return true;
-		case PLAYING:
-			return true;
-		default:
-			return false;
-		}
-	}
+	private void send(Transferable _data) {
 
-	@Override
-	protected void send(Transferable _data) {
-
-		if (!this.send(_data, 0)) {
-			this.client.disconnect();
-			this.running = false;
-			return;
-		}
+		if( !this.send(_data, 0) ) { this.client.disconnect(); return; }
 
 	}
 
-	@Override
-	protected boolean send(Transferable _data, int _triesCount) {
+	private boolean send(Transferable _data, int _triesCount) {
 
 		if (_triesCount >= this.exhaution) {
 			Debug.say("Sender Exhausted, preparing to stop.");
@@ -92,21 +81,14 @@ public class ClientSender extends AbstractSender {
 
 		Transferable data = _data;
 
-		try {
-			this.client.out.reset();
-		} catch (Exception _exception) {
-			return this.send(data, (_triesCount + 1));
-		}
-		try {
-			this.client.out.writeObject(data);
-		} catch (Exception _exception) {
-			return this.send(data, (_triesCount + 1));
-		}
-		try {
-			this.client.out.flush();
-		} catch (Exception _exception) {
-			return this.send(data, (_triesCount + 1));
-		}
+		try { this.client.out.reset(); }
+		catch (Exception _exception) { return this.send(data, (_triesCount + 1)); }
+
+		try { this.client.out.writeObject(data); }
+		catch (Exception _exception) { return this.send(data, (_triesCount + 1)); }
+
+		try { this.client.out.flush(); }
+		catch (Exception _exception) { return this.send(data, (_triesCount + 1)); }
 
 		return true;
 

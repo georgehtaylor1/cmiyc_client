bb9441cfcc018bc6b9ace06ac4c551b66dc58e99
George Taylor <george.h.taylor1@gmail.com>
2017-03-23 21:34:22 +0000

    Minor style changes and slider closes when game starts

--- src/sample/SlideScreen.java
+++ src/sample/SlideScreen.java
@@ -29,269 +29,262 @@ import states.ClientState;
 
 public class SlideScreen extends AnchorPane implements Observer {
 
-    private AnchorPane slider;
-    private HBox sliderControls;
-    private BorderPane together;
-    private TextField username;
-    private TextField host;
-    private Button connect;
-    private Button disconnect;
-    private Button exit;
-    private ToggleButton ready;
-    private Button mainButton;
-    private Button singlePlayer;
-    private Text text;
-    private Text text2;
-    private VBox connection;
-    private VBox settings;
-    
-    private State state;
+	private AnchorPane slider;
+	private HBox sliderControls;
+	private BorderPane together;
+	private TextField username;
+	private TextField host;
+	private Button connect;
+	private Button disconnect;
+	private Button exit;
+	private ToggleButton ready;
+	private Button mainButton;
+	private Button singlePlayer;
+	private Text text;
+	private Text text2;
+	private VBox connection;
+	private VBox settings;
 
-    private ToggleButton toggleButton2vs3;
-    private ToggleButton toggleButton1vs2;
-    private ToggleGroup group;
+	private State state;
 
-    private ToggleButton security;
-    private ToggleButton thief;
-    private ToggleGroup group2;
-    private GameScreen gameScreen;
-    
-    private boolean gameRendering;
+	private ToggleButton toggleButton2vs3;
+	private ToggleButton toggleButton1vs2;
+	private ToggleGroup group;
 
-    private TranslateTransition sliderTranslation;
+	private ToggleButton security;
+	private ToggleButton thief;
+	private ToggleGroup group2;
+	private GameScreen gameScreen;
 
-    public SlideScreen(GameScreen gameScreen) throws IOException  {
-    	this.state = State.START;
-        this.slider = new AnchorPane();
-        this.together = new BorderPane();
-        this.mainButton = new Button("Start");
-        this.sliderControls = new HBox();
-        this.exit = new Button("Exit");
-        this.username = new TextField();
-        this.host = new TextField();
-        this.connect = new Button("Connect");
-        this.disconnect = new Button("Disconnect");
-        this.singlePlayer = new Button("Single Player");
-        this.ready = new ToggleButton("Ready");
-        this.text = new Text();
-        text.setId("fancytext");
-        this.text2 = new Text();
-        text2.setId("fancytext");
-        this.connection = new VBox();
-        this.settings = new VBox();
-        this.gameRendering = false;
+	private boolean gameRendering;
 
-        //toggleButton for number of players
-        this.toggleButton1vs2 = new ToggleButton("1vs2");
-        this.toggleButton2vs3 = new ToggleButton("2vs3");
-        this.group = new ToggleGroup();
+	private TranslateTransition sliderTranslation;
 
-        this.security = new ToggleButton("Security");
-        this.thief = new ToggleButton("Thief");
-        this.group2 = new ToggleGroup();
+	public SlideScreen(GameScreen gameScreen) throws IOException {
+		this.state = State.START;
+		this.slider = new AnchorPane();
+		this.together = new BorderPane();
+		this.mainButton = new Button("Start");
+		this.sliderControls = new HBox();
+		this.exit = new Button("Exit");
+		this.username = new TextField();
+		this.host = new TextField();
+		this.connect = new Button("Connect");
+		this.disconnect = new Button("Disconnect");
+		this.singlePlayer = new Button("Single Player");
+		this.ready = new ToggleButton("Ready");
+		this.text = new Text();
+		text.setId("fancytext");
+		this.text2 = new Text();
+		text2.setId("fancytext");
+		this.connection = new VBox();
+		this.settings = new VBox();
+		this.gameRendering = false;
 
-        this.gameScreen = gameScreen;
-        this.drawScene();
-    }
+		//toggleButton for number of players
+		this.toggleButton1vs2 = new ToggleButton("1vs2");
+		this.toggleButton2vs3 = new ToggleButton("2vs3");
+		this.group = new ToggleGroup();
 
+		this.security = new ToggleButton("Security");
+		this.thief = new ToggleButton("Thief");
+		this.group2 = new ToggleGroup();
 
-    public void drawScene() {
-        slider.getStylesheets().add("styles/slider.css");
-        this.getStylesheets().add("styles/sliderLayer.css");
+		this.gameScreen = gameScreen;
+		this.drawScene();
+	}
 
-        sliderControls.setPrefHeight(40);
-        this.setPrefWidth(GameSettings.Arena.outerSize.getWidth());
-        this.setPrefHeight(GameSettings.Arena.outerSize.getHeight());
-        this.getChildren().addAll(slider, sliderControls);
-        sliderControls.getChildren().addAll(exit, mainButton);
-        sliderControls.setSpacing(1080);
+	public void drawScene() {
+		slider.getStylesheets().add("styles/slider.css");
+		this.getStylesheets().add("styles/sliderLayer.css");
 
-        slider.setPrefWidth(250);
-        slider.setPrefHeight(GameSettings.Arena.outerSize.getHeight());
-        username.setPromptText("username");
-        host.setPromptText("host");
+		sliderControls.setPrefHeight(40);
+		this.setPrefWidth(GameSettings.Arena.outerSize.getWidth());
+		this.setPrefHeight(GameSettings.Arena.outerSize.getHeight());
+		this.getChildren().addAll(slider, sliderControls);
+		sliderControls.getChildren().addAll(exit, mainButton);
+		sliderControls.setSpacing(1080);
 
+		slider.setPrefWidth(250);
+		slider.setPrefHeight(GameSettings.Arena.outerSize.getHeight());
+		username.setPromptText("username");
+		host.setPromptText("host");
 
-        connection.getChildren().addAll(username, host, connect, singlePlayer);
-        connection.setDisable(false);
-        HBox hBox1 = new HBox();
-        hBox1.getChildren().addAll(toggleButton1vs2, toggleButton2vs3);
-        HBox hBox2 = new HBox();
-        hBox2.getChildren().addAll(security, thief);
-        settings.getChildren().addAll(hBox1, hBox2);
-        together.setTop(connection);
-        together.setBottom(settings);
-        slider.getChildren().addAll(together);
-        settings.setSpacing(3.0);
-        connection.setSpacing(3.0);
+		connection.getChildren().addAll(username, host, connect, singlePlayer);
+		connection.setDisable(false);
+		HBox hBox1 = new HBox();
+		hBox1.getChildren().addAll(toggleButton1vs2, toggleButton2vs3);
+		HBox hBox2 = new HBox();
+		hBox2.getChildren().addAll(security, thief);
+		settings.getChildren().addAll(hBox1, hBox2);
+		together.setTop(connection);
+		together.setBottom(settings);
+		slider.getChildren().addAll(together);
+		settings.setSpacing(3.0);
+		connection.setSpacing(3.0);
 
+		AnchorPane.setBottomAnchor(sliderControls, 0.0);
+		AnchorPane.setRightAnchor(sliderControls, 0.0);
+		AnchorPane.setLeftAnchor(sliderControls, 0.0);
+		AnchorPane.setTopAnchor(slider, 0.0);
+		AnchorPane.setRightAnchor(slider, 0.0);
+		AnchorPane.setBottomAnchor(slider, sliderControls.getPrefHeight());
 
+		AnchorPane.setTopAnchor(together, 0.0);
+		AnchorPane.setBottomAnchor(together, 0.0);
+		AnchorPane.setLeftAnchor(together, 0.0);
+		AnchorPane.setRightAnchor(together, 0.0);
 
-        AnchorPane.setBottomAnchor(sliderControls, 0.0);
-        AnchorPane.setRightAnchor(sliderControls, 0.0);
-        AnchorPane.setLeftAnchor(sliderControls, 0.0);
-        AnchorPane.setTopAnchor(slider, 0.0);
-        AnchorPane.setRightAnchor(slider, 0.0);
-        AnchorPane.setBottomAnchor(slider, sliderControls.getPrefHeight());
+		sliderControls.setPrefWidth(GameSettings.Arena.outerSize.getWidth());
+		slider.setId("slider");
+		this.setId("sliderLayer");
+		sliderControls.setId("sliderControls");
+		mainButton.setId("mainButton");
+		exit.setId("exit");
+		username.setId("username");
+		host.setId("host");
+		connect.setId("connect");
+		disconnect.setId("connect");
+		ready.setId("connect");
+		singlePlayer.setId("connect");
+		toggleButton1vs2.setId("leftToggle");
+		toggleButton2vs3.setId("rightToggle");
+		security.setId("leftToggle");
+		thief.setId("rightToggle");
 
-        AnchorPane.setTopAnchor(together, 0.0);
-        AnchorPane.setBottomAnchor(together, 0.0);
-        AnchorPane.setLeftAnchor(together, 0.0);
-        AnchorPane.setRightAnchor(together, 0.0);
+		toggleButton2vs3.setToggleGroup(group);
+		toggleButton1vs2.setToggleGroup(group);
+		toggleButton1vs2.setSelected(true);
+		security.setToggleGroup(group2);
+		security.setSelected(true);
+		thief.setToggleGroup(group2);
 
-        sliderControls.setPrefWidth(GameSettings.Arena.outerSize.getWidth());
-        slider.setId("slider");
-        this.setId("sliderLayer");
-        sliderControls.setId("sliderControls");
-        mainButton.setId("mainButton");
-        exit.setId("exit");
-        username.setId("username");
-        host.setId("host");
-        connect.setId("connect");
-        disconnect.setId("connect");
-        ready.setId("connect");
-        singlePlayer.setId("connect");
-        toggleButton1vs2.setId("1vs2");
-        toggleButton2vs3.setId("2vs3");
-        security.setId("security");
-        thief.setId("thief");
+		username.setEditable(true);
 
-        toggleButton2vs3.setToggleGroup(group);
-        toggleButton1vs2.setToggleGroup(group);
-        toggleButton1vs2.setSelected(true);
-        security.setToggleGroup(group2);
-        security.setSelected(true);
-        thief.setToggleGroup(group2);
+		slider.setTranslateX(0);
+		sliderTranslation = new TranslateTransition(Duration.millis(400), slider);
 
-        username.setEditable(true);
+		sliderTranslation.setFromX(0);
+		sliderTranslation.setToX(this.getPrefWidth() - slider.getPrefWidth());
+		sliderTranslation.setRate(1);
+		sliderTranslation.play();
 
+		connect.setOnAction(e -> {
+			slideOut();
+			String[] _data = host.getText().split(":");
+			String _ip = _data[0];
+			int _port = Integer.parseInt(_data[1]);
+			String _name = username.getText();
+			gameScreen.client.connect(_port, _ip, _name);
+			if (!gameRendering) {
+				gameScreen.drawGame();
+				gameRendering = true;
+			}
+			setState(State.FIND);
+		});
 
-        slider.setTranslateX(0);
-        sliderTranslation = new TranslateTransition(Duration.millis(400), slider);
+		disconnect.setOnAction(e -> {
+			slideOut();
+			gameScreen.client.disconnect();
+			setState(State.START);
+		});
 
-        sliderTranslation.setFromX(0);
-        sliderTranslation.setToX(this.getPrefWidth() - slider.getPrefWidth());
-        sliderTranslation.setRate(1);
-        sliderTranslation.play();
+		security.setOnAction(e -> {
+			gameScreen.client.player.faction = Faction.SECURITY;
+		});
 
-        connect.setOnAction(e -> {
-        	String[] _data = host.getText().split(":");
-        	String _ip = _data[0];
-        	int _port = Integer.parseInt(_data[1]);
-        	String _name = username.getText();
-        	gameScreen.client.connect(_port, _ip, _name);
-        	if (!gameRendering) {
-        		gameScreen.drawGame();
-        		gameRendering = true;
-        	}
-            setState(State.FIND);
-        });
-        
-        disconnect.setOnAction(e -> {
-        	gameScreen.client.disconnect();
-        	setState(State.START);
-        });
-        
-        security.setOnAction(e -> {
-        	gameScreen.client.player.faction = Faction.SECURITY;
-        });
-        
-        thief.setOnAction(e -> {
-        	gameScreen.client.player.faction = Faction.THIEF;
-        });
-        
-        toggleButton1vs2.setOnAction(e -> {
-        	gameScreen.client.player.mode = GameMode.SHORT;
-        });
-        
-        toggleButton2vs3.setOnAction(e -> {
-        	gameScreen.client.player.mode = GameMode.LONG;
-        });
-        
-        singlePlayer.setOnAction(e -> {
-        	gameScreen.aiHandler.addPlayers(1, 1);
-        	gameScreen.aiHandler.start();
-        	gameScreen.drawGame();
-        });
+		thief.setOnAction(e -> {
+			gameScreen.client.player.faction = Faction.THIEF;
+		});
 
-        this.mainButton.setOnAction(e -> {
-            if(this.sliderTranslation.getRate() == 1) {
-                slideIn();
-            }
-        });
+		toggleButton1vs2.setOnAction(e -> {
+			gameScreen.client.player.mode = GameMode.SHORT;
+		});
 
-        this.exit.setOnAction(e -> {
-            gameScreen.aiHandler.end();
-            System.exit(0);
-        });
+		toggleButton2vs3.setOnAction(e -> {
+			gameScreen.client.player.mode = GameMode.LONG;
+		});
 
+		singlePlayer.setOnAction(e -> {
+			slideOut();
+			gameScreen.aiHandler.addPlayers(1, 1);
+			gameScreen.aiHandler.start();
+			gameScreen.drawGame();
+		});
 
-        this.setOnMouseClicked(event -> {
-            if(this.sliderTranslation.getRate() == -1) {
-                slideOut();
-            }
-        });
+		this.mainButton.setOnAction(e -> {
+			if (this.sliderTranslation.getRate() == 1) {
+				slideIn();
+			}
+		});
 
-    }
+		this.exit.setOnAction(e -> {
+			gameScreen.aiHandler.end();
+			System.exit(0);
+		});
 
-    public void slideIn(){
-        sliderTranslation.setRate(-1);
-        sliderTranslation.play();
-        this.setPrefWidth(slider.getPrefWidth());
-    }
+		this.setOnMouseClicked(event -> {
+			if (this.sliderTranslation.getRate() == -1) {
+				slideOut();
+			}
+		});
 
-    public void slideOut() {
-        sliderTranslation.setRate(1);
-        sliderTranslation.play();
-    }
+	}
 
-    public enum State {
-        START,
-        FIND,
-        LOBBY,
-        INGAME
-    }
+	public void slideIn() {
+		sliderTranslation.setRate(-1);
+		sliderTranslation.play();
+		this.setPrefWidth(slider.getPrefWidth());
+	}
 
-    public void setState(State state) {
-       switch (state) {
-           case START:
-        	   slider.getChildren().clear();
-        	   together.getChildren().clear();
-               together.setTop(connection);
-               together.setBottom(settings);
-        	   slider.getChildren().addAll(together);
-               mainButton.setText("Start");
-               break;
-           case FIND:
-        	   slider.getChildren().clear();
-        	   text.setText("Players found: 0");
-        	   VBox vbox1 = new VBox();
-        	   vbox1.getChildren().add(text);
-        	   vbox1.getChildren().add(disconnect);
-        	   together.getChildren().clear();
-               together.setTop(vbox1);
-               slider.getChildren().add(together);
-               mainButton.setText("Finding...");
-               break;
-           case LOBBY:
-               mainButton.setText("Menu");
-               slider.getChildren().clear();
-               text.setText("Players found: " + this.gameScreen.client.obData.getPlayers());
-        	   slider.getChildren().add(text);
-               slider.getChildren().add(ready);
-               break;
-           case INGAME:
-               mainButton.setText("Menu");
-               slider.getChildren().clear();
-               text.setText("Thieves Captured: 0");
-               text2.setText("Thieves Escaped: 0");
-               slider.getChildren().add(text);
-               slider.getChildren().add(text2);
-               slider.getChildren().add(disconnect);
-               break;
-       }
-    }
+	public void slideOut() {
+		sliderTranslation.setRate(1);
+		sliderTranslation.play();
+	}
 
+	public enum State {
+		START, FIND, LOBBY, INGAME
+	}
+
+	public void setState(State state) {
+		switch (state) {
+		case START:
+			slider.getChildren().clear();
+			together.getChildren().clear();
+			together.setTop(connection);
+			together.setBottom(settings);
+			slider.getChildren().addAll(together);
+			mainButton.setText("Start");
+			break;
+		case FIND:
+			slider.getChildren().clear();
+			text.setText("Players found: 0");
+			VBox vbox1 = new VBox();
+			vbox1.getChildren().add(text);
+			vbox1.getChildren().add(disconnect);
+			together.getChildren().clear();
+			together.setTop(vbox1);
+			slider.getChildren().add(together);
+			mainButton.setText("Finding...");
+			break;
+		case LOBBY:
+			mainButton.setText("Menu");
+			slider.getChildren().clear();
+			text.setText("Players found: " + this.gameScreen.client.obData.getPlayers());
+			slider.getChildren().add(text);
+			slider.getChildren().add(ready);
+			break;
+		case INGAME:
+			mainButton.setText("Menu");
+			slider.getChildren().clear();
+			text.setText("Thieves Captured: 0");
+			text2.setText("Thieves Escaped: 0");
+			slider.getChildren().add(text);
+			slider.getChildren().add(text2);
+			slider.getChildren().add(disconnect);
+			break;
+		}
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
@@ -300,13 +293,9 @@ public class SlideScreen extends AnchorPane implements Observer {
 				this.setState(State.INGAME);
 			else
 				this.setState(State.LOBBY);
-		}
-		else if (this.state == State.INGAME) {
+		} else if (this.state == State.INGAME) {
 			if (this.gameScreen.client.obData.getState() == ClientState.POSTGAME)
 				this.state = State.START;
 		}
 	}
 }
-
-
-
